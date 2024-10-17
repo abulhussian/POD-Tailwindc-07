65eae62f17c85ee5fa6bf98d4dbe14e11990a0f5
abulhussian12 <abulhussian1229@gmail.com>
2024-10-17 08:37:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        dblue:'#121725',
+        green:'#54E6AF',
+        white:'#FFFFFF',
+        grey:'#C2CBE5',
+        lblue:'#2C344B',
+        lgreen:'#B3FFE2'
+      },
+      backgroundImage:{
+        bgImage:"url('podcast-tailwind-css-07/pod-request-access-landing-page/pod-request-access-landing-page/starter-code/assets/mobile/image-host.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
